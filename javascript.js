6407d7a0e5cd000975bf17ce413b420a531e3fd9
Youki Lewis <54418235+YolkiNii@users.noreply.github.com>
2025-12-05 21:41:40 -0600

    feat: add size popup and opacity build up

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const grid = document.getElementById("grid-container");
+const sizeButton = document.querySelector("button");
 
 function createGrid(size) {
     for (let i = 0; i < size; i++) {
@@ -16,12 +17,50 @@ function createGrid(size) {
     return;
 }
 
+function generateRandomBackground() {
+    // exclude white: rgb(255, 255, 255)
+    const r = Math.floor(Math.random() * 254)
+    const g = Math.floor(Math.random() * 254)
+    const b = Math.floor(Math.random() * 254)
+
+    return `rgb(${r} ${g} ${b} / 10%)`
+}
+
 createGrid(16);
 
 grid.addEventListener("mouseover", (event) => {
     const square = event.target;
 
     if (square.classList.contains("square")) {
-        square.classList.add("hover-color");
+        const background = square.style["background"];
+        
+        if (!background)
+            square.style["background"] = generateRandomBackground();
+        else {
+            // extract the rgb values
+            const rgba = background.slice(background.indexOf("(") + 1, -1).split(", ");
+            let opacity = +rgba[3];
+
+            if (opacity < 1)
+                opacity += 0.1;
+
+            square.style["background"] = `rgba(${rgba[0]}, ${rgba[1]}, ${rgba[2]}, ${opacity})`
+        }
     }
+});
+
+sizeButton.addEventListener("click", (event) => {
+    let chosenSize = 0;
+
+    while (chosenSize < 1 || chosenSize > 100 || isNaN(chosenSize)) {
+        chosenSize = prompt("Select a size between 1 and 100.");
+
+        chosenSize = parseInt(chosenSize);
+    }
+
+    // clear grid
+    while (grid.firstChild)
+        grid.removeChild(grid.firstChild);
+
+    createGrid(chosenSize)
 });
